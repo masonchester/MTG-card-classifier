a9677557353fe5a0341557ebbe599541c54f9131
Christopher Sarzen <87316434+canweffplease@users.noreply.github.com>
2023-11-10 16:54:36 -0500

    Update processCards.py

--- src/Preprocessing Pipeline/processCards.py
+++ src/Preprocessing Pipeline/processCards.py
@@ -4,19 +4,24 @@ with open("scryfall_default_cards.json", 'r', encoding='utf-8') as f:
     cards = json.load(f)
 
 processed_cards = []
-for card in cards:
-    if 'image_uris' in card and 'art_crop' in card['image_uris'] and 'colors' in card and 'type_line' in card:
-        
-        primary_type = card['type_line'].split("—")[0].strip()
+types = ['Planeswalker', 'Creature', 'Instant', 'Sorcery', 'Enchantment', 'Artifact', 'Land']
 
-        processed_card = {
-            'image_url': card['image_uris']['art_crop'],
-            'colors': card['colors'],
-            'type': primary_type
-        }
-        processed_cards.append(processed_card)
+for card in cards:
+    if 'image_uris' in card and 'art_crop' in card['image_uris'] and 'type_line' in card:
+        card_colors = card.get('colors', [])
+        if len(card_colors) == 0:
+            card_colors = ["C"]
+        if len(card_colors) == 1:
+            primary_type = card['type_line'].split("—")[0].strip()
+            if primary_type in types:
+                processed_card = {
+                    'image_url': card['image_uris']['art_crop'],
+                    'colors': card_colors,
+                    'type': primary_type
+                }
+                processed_cards.append(processed_card)
 
 with open("processed_cards.json", 'w', encoding='utf-8') as f:
     json.dump(processed_cards, f, indent=4)
 
-print(f"Saved {len(processed_cards)} processed cards to processed_cards.json")
+print(f"Saved {len(processed_cards)} cards to processed_cards.json")

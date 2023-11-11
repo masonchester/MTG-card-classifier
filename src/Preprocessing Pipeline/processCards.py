9cf93e5ff3c0378d47f0bd839a7ec9a7e0f86f78
Christopher Sarzen <87316434+canweffplease@users.noreply.github.com>
2023-11-11 17:07:26 -0500

    Update processCards.py
    highres comparison added

--- src/Preprocessing Pipeline/processCards.py
+++ src/Preprocessing Pipeline/processCards.py
@@ -3,30 +3,31 @@ import json
 with open("scryfall_default_cards.json", 'r', encoding='utf-8') as f:
     cards = json.load(f)
 
-processed_cards = []
-unique_card_names = set()
+processed_cards_dict = {}
 types = ['Planeswalker', 'Creature', 'Instant', 'Sorcery', 'Enchantment', 'Artifact', 'Land']
 
 for card in cards:
-    if 'image_uris' in card and 'art_crop' in card['image_uris'] and 'type_line' in card and 'name' in card:
+    if 'image_uris' in card and 'art_crop' in card['image_uris'] and 'type_line' in card:
         card_name = card['name']
-        if card_name not in unique_card_names:
-            card_colors = card.get('colors', [])
-            if len(card_colors) == 0:
-                card_colors = ["C"]
+        primary_type = card['type_line'].split("—")[0].strip()
+        highres = card.get('highres_image', False)
+        
+        if primary_type in types:
+            card_colors = card.get('colors', ["C"]) if card.get('colors') else ["C"]
             if len(card_colors) == 1:
-                primary_type = card['type_line'].split("—")[0].strip()
-                if primary_type in types:
+                if card_name not in processed_cards_dict or (card_name in processed_cards_dict and highres):
                     processed_card = {
                         'name': card_name,
                         'image_url': card['image_uris']['art_crop'],
                         'colors': card_colors,
-                        'type': primary_type
+                        'type': primary_type,
+                        'highres_image': highres
                     }
-                    processed_cards.append(processed_card)
-                    unique_card_names.add(card_name)
+                    processed_cards_dict[card_name] = processed_card
+
+processed_cards = list(processed_cards_dict.values())
 
 with open("processed_cards.json", 'w', encoding='utf-8') as f:
     json.dump(processed_cards, f, indent=4)
 
-print(f"Saved {len(processed_cards)} UNIQUE!!!!!! cards to processed_cards.json")
+print(f"Saved {len(processed_cards)} unique cards to processed_cards.json")

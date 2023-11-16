4857786e6d5bca8a2e7db5b5dee2fa1cf296a3f5
masonchester <110614975+masonchester@users.noreply.github.com>
2023-11-16 17:42:44 -0500

    updated model and dataset gen

--- src/classifier.py
+++ src/classifier.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import pandas as pd
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 train_path = "images/train"
@@ -38,21 +39,36 @@ def gen_dataframe_from_dir (dir_path):
 def gen_dataset(df_X):
     '''
     generates an X and y dataset consisting of image files 
-    paths in X and one-hot encoded labels in y.
+    paths in X and one-hot encoded labels in y and zips them in a dataset object.
     '''
-    data_X = df_X.drop('labels', axis=1)
+    data_X = df_X['image paths']
+    data_X = tf.data.Dataset.from_tensor_slices(data_X)
+    data_X = data_X.map(load_and_preprocess_image)
+
     data_y = df_X['labels'].str.get_dummies(', ')
-    return data_X, data_y
+    data_y = tf.data.Dataset.from_tensor_slices(data_y)
+
+    dataset = tf.data.Dataset.zip((data_X, data_y))
+    dataset = dataset.batch(64)
+    return dataset
+
+def load_and_preprocess_image(path):
+    '''
+    loads the images and resizes them to 299 x 299
+    '''
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [299, 299])
+    return image
 
 train_df = gen_dataframe_from_dir(train_path)
 test_df = gen_dataframe_from_dir(test_path)
 val_df = gen_dataframe_from_dir(val_path)
 
-train_X, train_y = gen_dataset(train_df)
-test_X, test_y = gen_dataset(test_df)
-val_X, val_y = gen_dataset(val_df)
+train_data = gen_dataset(train_df)
+test_data  = gen_dataset(test_df)
+val_data = gen_dataset(val_df)
 
-print(train_y.sample(5))
 
 base_model = tf.keras.applications.resnet_v2.ResNet152V2(
     include_top=False,
@@ -63,9 +79,9 @@ base_model = tf.keras.applications.resnet_v2.ResNet152V2(
 base_model.trainable = False
 
 top_model = tf.keras.Sequential([
-     tf.keras.layers.Flatten(),
+     tf.keras.layers.GlobalAveragePooling2D(),
      tf. keras.layers.Dense(1024, activation='relu'),  
-     tf.keras.layers.Dense(36, activation='softmax')
+     tf.keras.layers.Dense(12, activation='softmax')
 ])
 
 model = tf.keras.models.Sequential([
@@ -75,4 +91,4 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=train_X, y=train_y, epochs=10, validation_data=(val_X, val_y))
+model.fit(x=train_data, epochs=10, validation_data=val_data)

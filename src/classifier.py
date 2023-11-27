71207eb612ad56e604ed0bc3f752b9420a736ba1
KoT3sts <85979064+KoT3sts@users.noreply.github.com>
2023-11-27 16:51:54 -0500

    Update classifier.py

--- src/classifier.py
+++ src/classifier.py
@@ -1,26 +1,23 @@
-
 import tensorflow as tf
 import pandas as pd
 import os
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import precision_score, recall_score
 import matplotlib.pyplot as plt
+import time
 
 train_path = "images/train"
 test_path = "images/test"
 val_path = "images/validation"
 
-def gen_dataframe_from_dir (dir_path):
-    '''
-    generates a dataframe containing the image path and 
-    labels from the directory structure.
-    '''
+def gen_dataframe_from_dir(dir_path):
     labels = list()
     image_paths = list()
 
     for class_name in os.listdir(dir_path):
         class_dir = os.path.join(dir_path, class_name)
         if os.path.isdir(class_dir):
-             for filename in os.listdir(class_dir):
+            for filename in os.listdir(class_dir):
                 file_dir = os.path.join(class_dir, filename)
                 if os.path.isfile(file_dir):
                     image_paths.append(file_dir)
@@ -28,18 +25,14 @@ def gen_dataframe_from_dir (dir_path):
                     labels.append(str)
 
     df = pd.DataFrame({
-        "image paths":image_paths, 
-        "labels":labels, 
-          })
+        "image paths": image_paths,
+        "labels": labels,
+    })
 
     print(df.sample(5))
     return df
-   
-def gen_dataset(df_X):
-    '''
-    generates an X and y dataset consisting of image files 
-    paths in X and one-hot encoded labels in y and zips them in a dataset object.
-    '''
+
+def gen_dataset(df_X, augment=False):
     data_X = df_X['image paths']
     data_y = df_X['labels']
 
@@ -47,15 +40,27 @@ def gen_dataset(df_X):
     data_y = mlb.fit_transform(data_y)
 
     dataset = tf.data.Dataset.from_tensor_slices((data_X, data_y))
+
+    if augment:
+        # Data augmentation
+        data_augmentation = tf.keras.preprocessing.image.ImageDataGenerator(
+            rotation_range=20,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest'
+        )
+
+        dataset = dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
+
     dataset = dataset.map(load_and_preprocess_image)
     dataset = dataset.batch(64)
 
     return dataset
 
 def load_and_preprocess_image(image, label):
-    '''
-    loads the images and resizes them to 299 x 299
-    '''
     image = tf.io.read_file(image)
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.resize(image, [299, 299])
@@ -71,10 +76,9 @@ train_df = train_df.sample(frac=1)
 test_df = test_df.sample(frac=1)
 val_df = val_df.sample(frac=1)
 
-train_data = gen_dataset(train_df)
-test_data  = gen_dataset(test_df)
+train_data = gen_dataset(train_df, augment=True)
+test_data = gen_dataset(test_df)
 val_data = gen_dataset(val_df)
-
 
 base_model = tf.keras.applications.resnet_v2.ResNet50V2(
     include_top=False,
@@ -82,7 +86,10 @@ base_model = tf.keras.applications.resnet_v2.ResNet50V2(
     input_shape=(299, 299, 3),
 )
 
-base_model.trainable = False
+base_model.trainable = True
+fine_tune_at = 100  # Adjust this value based on the layer you want to fine-tune from
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
 
 top_model = tf.keras.Sequential([
      tf.keras.layers.Flatten(),
@@ -92,12 +99,60 @@ top_model = tf.keras.Sequential([
      tf.keras.layers.Dense(6, activation='softmax')
 ])
 
-model = tf.keras.models.Sequential([                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+model = tf.keras.models.Sequential([
     base_model,
     top_model
 ])
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+# Learning rate scheduling
+initial_learning_rate = 0.001
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate, decay_steps=10000, decay_rate=0.9, staircase=True
+)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+
+# Compile the model
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=train_data, epochs=10, validation_data=val_data)
+# Early stopping
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=3,
+    restore_best_weights=True
+)
+
+# Training with data augmentation and early stopping
+history = model.fit(x=train_data, epochs=10, validation_data=val_data, callbacks=[early_stopping])
+
+# Extracting metrics
+training_accuracy = history.history['accuracy'][-1]
+validation_accuracy = history.history['val_accuracy'][-1]
+loss = history.history['loss'][-1]
+validation_loss = history.history['val_loss'][-1]
+
+print(f'Training Accuracy: {training_accuracy * 100:.2f}%')
+print(f'Validation Accuracy: {validation_accuracy * 100:.2f}%')
+print(f'Training Loss: {loss:.4f}')
+print(f'Validation Loss: {validation_loss:.4f}')
+
+# Evaluation metrics for precision and recall
+predictions = model.predict(test_data)
+predicted_labels = mlb.inverse_transform(predictions)
+true_labels = mlb.inverse_transform(test_data.map(lambda x, y: y))
+
+predicted_labels = [label for sublist in predicted_labels for label in sublist]
+true_labels = [label for sublist in true_labels for label in sublist]
+
+precision = precision_score(true_labels, predicted_labels, average='weighted')
+recall = recall_score(true_labels, predicted_labels, average='weighted')
+
+print(f'Precision: {precision:.4f}')
+print(f'Recall: {recall:.4f}')
+
+# Processing Time
+start_time = time.time()
+model.fit(x=train_data, epochs=10, validation_data=val_data, callbacks=[early_stopping])
+end_time = time.time()
+processing_time = end_time - start_time
+print(f'Processing Time: {processing_time:.2f} seconds')
